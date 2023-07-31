d1ced55b711b1689f6cd0c6683af6763cdb4d8d6
Paulo Coutinho <pcmnac@gmail.com>
2023-07-31 14:33:35 +0100

    🔥 removed worker

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,6 @@ import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import registerServiceWorker from './registerServiceWorker';
 import './index.sass';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
